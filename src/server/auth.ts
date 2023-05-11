7c36447e11e10b34be506fc1849b1ed00de7450b
Harit Joshi <joshiharit3@gmail.com>
2023-05-11 16:39:24 +0530

    init setup

---
+++ src/server/auth.ts
@@ -0,0 +1,44 @@
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type NextAuthOptions,
+  type DefaultSession,
+} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "~/server/db";
+
+
+export const authOptions: NextAuthOptions = {
+  session: { strategy: "jwt" },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials", // demo name of the form
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+
+      // eslint-disable-next-line @typescript-eslint/require-await
+      async authorize(credentials, req) {
+        // Dummy authorize setup
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+        if (user) return user;
+        else return null;
+      },
+    }),
+  ],
+};
+
+/**
+ * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
+ *
+ * @see https://next-auth.js.org/configuration/nextjs
+ */
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
+};
